fce382accb7b5bc2af38ae24df63c29a980d7a05
Colin Goltra <colin@goltra.co>
2016-05-02 19:27:23 -0700

    omg please just work...

---
+++ react-native-todo/application/components/ToDoEdit.js
@@ -0,0 +1,54 @@
+'use strict';
+var styles = require('../styles/styles');
+var React = require('react-native');
+var t = require('tcomb-form-native');
+var { View, TouchableHighlight, Text } = React;
+var Form = t.form.Form;
+
+var ToDo = t.struct({txt: t.Str, complete: t.Bool});
+
+var options = {
+    fields: {
+        txt: {
+            label: 'To-Do Item',
+            placeholder: 'enter a to do item here',
+            autoFocus: true
+        }
+    }
+};
+
+class ToDoEdit extends React.Component {
+    constructor() {
+        super();
+        this.onUpdate = this.onUpdate.bind(this);
+    }
+
+    onUpdate() {
+        var value = this.refs.form.getValue();
+        if (value) {
+            this.props.update(value, this.props.id);
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.todo}>
+                <Form
+                    ref="form"
+                    type={ToDo}
+                    onChange={this._onChange}
+                    options={options}
+                    value={this.props.item}/>
+                <TouchableHighlight
+                    style={[styles.button, styles.saveButton]}
+                    onPress={this.onUpdate}
+                    underlayColor='#99d9f4'>
+                    <Text style={styles.buttonText}>Save</Text>
+                </TouchableHighlight>
+            </View>
+        )
+    }
+}
+
+
+module.exports = ToDoEdit;
