3d9949f161ece2ddbd2a15f92aee8635a83b3af8
David Ogor <doctown@yahoo.com>
2016-05-10 11:25:03 -0700

    Initial template for test suite

---
+++ react-native-todo/application/components/TestSuite.js
@@ -0,0 +1,9 @@
+'use strict';
+var styles = require('../styles/styles');
+var scripts = require('../helpers/scripts');
+var React = require('react-native');
+var ToDoListItem = require('./ToDoListItem');
+var {
+  View, TouchableHighlight, Text
+  } = React;
+import realm from './realm';
