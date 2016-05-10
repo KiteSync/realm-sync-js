5f853828681d97a6d6746a00d1282eac32c84998
David Ogor <doctown@yahoo.com>
2016-05-10 12:05:18 -0700

    Add button to run test

--- react-native-todo/application/components/TestSuite.js
+++ react-native-todo/application/components/TestSuite.js
@@ -1,9 +1,30 @@
 'use strict';
 var styles = require('../styles/styles');
-var scripts = require('../helpers/scripts');
 var React = require('react-native');
-var ToDoListItem = require('./ToDoListItem');
+var tests = require('../helpers/tests');
 var {
   View, TouchableHighlight, Text
   } = React;
 import realm from './realm';
+
+class TestSuite extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <View style = {styles.container}>
+        <Text style={styles.button} onPress={tests.runTests}>
+          Tap to Run All Tests
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    )
+  }
+}
+
+module.exports = TestSuite;
--- react-native-todo/application/components/ToDoListContainer.js
+++ react-native-todo/application/components/ToDoListContainer.js
@@ -6,6 +6,7 @@ var ToDoEdit = require('./ToDoEdit');
 var FileSystemTests = require('./FileSystemTests');
 var RealmDbTests = require('./RealmDbTests');
 var RemoteDbTests = require('./RemoteDbTests');
+var TestSuite = require('./TestSuite');
 var { Text, View, ListView, TouchableHighlight, AlertIOS } = React;
 const Realm = require('realm');
 
@@ -99,6 +100,14 @@ class ToDoContainer extends React.Component {
         });
     }
 
+    openTestSuite() {
+        this.props.navigator.push({
+            title: 'Testing Suite',
+            component: TestSuite,
+            passProps: {realmPath: this.state.realmPath}
+        });
+    }
+
     render() {
         return (
             <View style={{flex:1}}>
@@ -127,6 +136,13 @@ class ToDoContainer extends React.Component {
                     underlayColor='#99d9f4'
                     onPress={this.openFileSystemTests.bind(this)}>
                     <Text style={styles.buttonText}>Test filesystem</Text>
+                </TouchableHighlight>
+
+                <TouchableHighlight
+                  style={[styles.button, styles.newButton]}
+                  underlayColor='#00ff00'
+                  onPress={this.openTestSuite.bind(this)}>
+                    <Text style={styles.buttonText}>Auto Test Suite</Text>
                 </TouchableHighlight>
 
                 <TouchableHighlight
