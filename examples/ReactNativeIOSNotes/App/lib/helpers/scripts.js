47a06f5efcda56032c0e6798860d4df0654f763d
Colin Goltra <colin@goltra.co>
2016-05-18 15:18:44 -0700

    conformed lib and deleted icon files

--- examples/ReactNativeIOSNotes/App/lib/helpers/scripts.js
+++ examples/ReactNativeIOSNotes/App/lib/helpers/scripts.js
@@ -2,38 +2,47 @@
 import usnHandler from './usnHandler';
 
 /**
- * description here
- * @param  something here
- * @post
- * @return something here
-*/
+ * Adds or updates an object in the syncQueue when
+ * an object is added to the database
+ * @param realm: database object
+ * @param {string} database class
+ * @param {obj} database class updates
+ */
+var addObjectToSyncQueue = function(realm, type, obj) {
+  // TODO: Only search syncQueue for object to update for updates
 
-var addObjectToSyncQueue = function(realm, type, obj, realmParam) {
-  var returnObj = {}
-  returnObj.usn = usnHandler.incrementAndReturnUsn();
-  returnObj.realmSyncId = obj.realmSyncId;
-  returnObj.type = type;
-  returnObj.modified = Date.now();
-  var body = {}
-  for(var key in obj) {
-    body[key] = obj[key];
+  var filterText = 'realmSyncId = "' + obj.realmSyncId + '"'
+  let objToUpdate = realm.objects('SyncQueue').filtered(filterText);
+
+  var returnObj = {
+    usn: usnHandler.incrementAndReturnUsn(),
+    realmSyncId: obj.realmSyncId,
+    type: type,
+    modified: Date.now(),
+    body: JSON.stringify(obj)
   }
-  returnObj.body = JSON.stringify(body);
-    try {
-      // Added to work with RealmSync class
-      if (realmParam) {
-        let syncQueue = realmParam.create('SyncQueue', returnObj);
-      } else {
-        let syncQueue = realm.create('SyncQueue', returnObj);
+
+  try {
+    if(objToUpdate.length === 0) {
+      realm.create('SyncQueue', returnObj);
+    } else {
+      for(key in returnObj) {
+        objToUpdate[0][key] = returnObj[key];
       }
-    } catch(error) {
-      console.log("ERROR in syncQueue write", error);
     }
+  } catch(error) {
+    console.log("ERROR in syncQueue write", error);
+  }
 }
 
 
-
-var deleteObjFromLocalChanges = function(realm, realmSyncId) {
+/**
+ * Updates USN and Removes the body from item in syncQueue \
+ * to indicate its been deleted.
+ * @param realm database object
+ * @param {string} realm sync id
+ */
+var markSyncQueueObjectAsDeleted = function(realm, realmSyncId) {
   var filterText = 'realmSyncId = "' + realmSyncId + '"'
   let objToDelete = realm.objects('SyncQueue').filtered(filterText);
 
@@ -43,11 +52,15 @@ var deleteObjFromLocalChanges = function(realm, realmSyncId) {
 }
 
 
-
+/**
+ * Generates a 'unique' id.
+ * Used for initalizing a new db object with a realmSyncId
+ * @returns {string} eg: "1A2EE1F7-45F7-E5F4-244C"
+ */
 var generateGuid = function() {
   var result, i, j;
   result = '';
-  for(j=0; j<16; j++) {
+  for(j=0; j<20; j++) {
     if( j == 8 || j == 12|| j == 16|| j == 20)
       result = result + '-';
     i = Math.floor(Math.random()*16).toString(16).toUpperCase();
@@ -55,16 +68,10 @@ var generateGuid = function() {
   }
   return result;
 }
-var names = ["Afghanistan", "Åland Islands", "Albania", "Algeria", "American Samoa", "AndorrA", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, The Democratic Republic of the", "Cook Islands", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia"];
 
-var randomName = function() {
-  var randomIndex = Math.floor(Math.random() * names.length);
-  return names[randomIndex];
-}
 
 module.exports = {
   addObjectToSyncQueue: addObjectToSyncQueue,
-  deleteObjFromLocalChanges: deleteObjFromLocalChanges,
+  markSyncQueueObjectAsDeleted: markSyncQueueObjectAsDeleted,
   generateGuid: generateGuid,
-  randomName: randomName
 }
